2fdc6aedbabeffa19b8670beba82041224367419
Danil Mazurkin <mazurkin.daniel@gmail.com>
2022-10-18 17:12:40 +1000

    Add UserTable in database

--- data/database_init.go
+++ data/database_init.go
@@ -6,11 +6,17 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func InitDatabase(db *sql.DB) {
+	user := new(UserStruct)
+	user.CreateTableUser(db)
+}
+
 func OpenDatabase() (db *sql.DB, err error) {
 	db, err = sql.Open("sqlite3", "database.db")
 
 	if err == nil {
 		fmt.Println("Connecting with database")
+		InitDatabase(db)
 	} else {
 		fmt.Println("Error with connecting ", err)
 	}
--- data/user.go
+++ data/user.go
@@ -1,11 +1,36 @@
 package data
 
-type User interface {
-	createTableUser()
-	createUser()
+import (
+	"database/sql"
+	"fmt"
+)
+
+type UserInterface interface {
+	CreateTableUser()
+	CreateUser()
 }
 
-type user struct {
+type UserStruct struct {
 	Username string `json:username`
 	Password string `json:password`
+}
+
+func (*UserStruct) CreateTableUser(db *sql.DB) {
+	_, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS users(
+			   ID INT PRIMARY KEY     NOT NULL,
+			   NAME           TEXT    NOT NULL,
+			   PASSWORD        CHAR(50)
+			);`,
+	)
+
+	if err != nil {
+		fmt.Println("Error with creating table", err)
+	} else {
+		fmt.Println("Table was created")
+	}
+}
+
+func (*UserStruct) CreateUser(Username string, Password string) {
+
 }
